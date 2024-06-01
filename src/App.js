0fda5c18d9e1ba0c2fd1ef1c69dd349b3e705515
leeeeejieun <arjleun19@gmail.com>
2024-06-02 02:55:03 +0900

    링크 클릭 시 이미지&내용 변경

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import {Header, Nav, Article} from './main.js'
+import { useState } from 'react';
+import './App.css'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+const topics =  [
+  {id: 1,title : "html", body: "html is ...", img: "./img/html.png"},
+  {id: 2,title : "css", body: "css is ...", img: "./img/css.png"},
+  {id: 3,title : "javascript", body: "js is ...", img: "./img/js.png"}
+];
+
+
+const App = ()=>{
+  const [id, setId] = useState(null);
+  const [mode, setMode] = useState("Welcome");
+  let img,content = null;
+
+  if(mode === "Welcome"){
+    img = require('./img/web.png');
+    content = <Article title="welcome" body="Hello, WEB"/>
+  }
+
+  else if(mode === "Read"){
+      topics.map((topic)=>{
+        if(id === topic.id){
+          img = require(`${topic.img}`);
+          content = <Article title={topic.title} body={topic.body}/>
+          console.log(topic.id)
+          }
+        }
+      )
+  }
+
+  return(
+    <>
+      <Header onChangeMode={()=>
+        setMode("Welcome")
+      }/>
+      <Nav title={topics} onChangeMode={(id)=>{
+        setMode("Read");
+        setId(id);
+        console.log(id);
+      }}/>
+      <div className='wrapper'>
+        <img src={img}/>
+        {content}
+      </div>
+    </>
   );
 }
 
 export default App;
+
