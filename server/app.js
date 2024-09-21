17ca7b910d766d0cb8a84995bbacaf5cf6c8571f
leeeeejieun <arjleun19@gmail.com>
2024-09-22 05:36:21 +0900

    게시글 추가 API 테스트 완료
    - 유효성 검증 및 파일 크기 제한 기능은 추가 예정

--- server/app.js
+++ server/app.js
@@ -17,6 +17,7 @@ app.use(cors({
     origin: 'http://localhost:3000', // 클라이언트 도메인
     credentials: true // 쿠키와 같은 인증 정보를 허용
 }));
+app.use(express.static(`${__dirname}/src/public`));   // 정적 파일 제공 경로
 app.use(cookieParser());                     
 app.use(express.json());     
 app.use(express.urlencoded({ extended: false }));  
